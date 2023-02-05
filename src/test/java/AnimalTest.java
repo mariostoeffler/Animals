4f085a963437d40447cd5b430e3c0733fda46808
mariostoeffler <52000392@edu.campus02.at>
2023-02-05 15:36:39 +0100

    new Project created;

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 class AnimalTest {
+
+    Animal animal = new Animal();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        animal.setName("Leo");
+        animal.setSpecies("Löwe");
+        animal.setAge(3);
+        animal.setAnimalLoud("Roarr");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void giveAnimalLoud() {
+        String result="Roarr";
+        Assertions.assertEquals(result, animal.giveAnimalLoud());
+
+    }
 
 }
